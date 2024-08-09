308b9d4f2f4a6982f8ea5fda508f601f34817d5d
Adwitiya Paul <adwitiya.paul2104@gmail.com>
2024-08-09 22:53:36 +0530

    fix: added contact us and fixed reset password

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,7 +5,8 @@ import {
   get_all_courses,
   get_course,
   delete_course,
-} from "../controllers/admin";
+  contact_us,
+} from "../controllers/admin.js";
 
 const router = express.Router();
 
@@ -14,5 +15,6 @@ router.get("/", get_course);
 router.get("/:id", get_all_courses);
 router.patch("/:id", update_course);
 router.delete("/:id", delete_course);
+router.post("/contact", contact_us);
 
 export default router;
